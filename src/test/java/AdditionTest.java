7da488d528a3b2f8ef5f1d03322b62e93979d081
Madhura <valkemadhura@gmail.com>
2022-09-26 23:28:38 +0530

    Test demo committed

---
+++ src/test/java/AdditionTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+public class AdditionTest {
+
+    @org.junit.Test
+    public void add() {
+        int result=Addition.add(4,3);
+        int correct=12;
+
+
+    }
+}
